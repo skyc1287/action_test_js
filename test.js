2f6d2b6537ea766a4bcc86ff1e09557e3b49582a
skyc1287 <sky.c.cerv@gmail.com>
2023-11-02 11:47:11 -0600

    add test.js file

---
+++ test.js
@@ -0,0 +1,17 @@
+function addNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+function generateRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    return randomNumber;
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const randomNumber = generateRandomNumber();
+    console.log(randomNumber);
+});
